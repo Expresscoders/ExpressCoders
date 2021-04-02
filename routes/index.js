60dea1716e94b94932f494e693fd0f39b8e35155
Diwakar23 <diwakar232496@gmail.com>
2021-04-02 14:49:37 -0400

    changes in routes

--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,60 @@ router.get("/surveys", (req,res)=>{
   })
 })
 
+//add
+router.post("/add", (req,res)=>{
+  let newsurveyItem = new surveyItem({
+    surveyName: req.body.surveyName,
+    option1: req.body.option1,
+    option2: req.body.option2
+  })
+  newsurveyItem.save((err,item)=>{
+    if(err){
+      res.json(err)
+    }else{
+      res.json({message:"Survey Added"})
+    }
+  })
+})
 
+//update
+router.post("/update/:id", (req, res)=>{
+  let id = mongoose.Types.ObjectId(req.params.id)
+  surveyItem.findOneAndUpdate({_id: id},{
+    $set:{
+      surveyName : req.body.surveyName,
+      option1 : req.body.option1,
+      option2: req.body.option2
+    }
+  },(err,result)=>{
+    if(err){
+      res.json(err)
+    }else{
+      res.json(result)
+    }
+  })
+})
+
+//get Survey By ID
+router.get("/edit/:id",(req,res)=>{
+  let id = mongoose.Types.ObjectId(req.params.id)
+  surveyItem.findById(id, (err,item)=>{
+    res.json(item)
+  })
+})
+
+//delete
+router.delete("/delete/:id", (req,res)=>{
+  let id = mongoose.Types.ObjectId(req.params.id)
+  surveyItem.remove({_id: (id)}, (err,result)=>{
+    if(err){
+      res.json(err)
+    }else{
+      res.json(result)
+    }
+  })
+})
+
+
+module.exports = router;
 
